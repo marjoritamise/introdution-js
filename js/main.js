d4883ae941034d0635c2ee744871e34551d08301
Márjori Tamise <marjori.tamise@corebiz.com.br>
2020-03-29 14:30:56 -0300

    comentarios da aula

--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
+// console.log() para mensagens no console
+// alert() para mensagens com popup (pouco utilizado)
+// querySelector serve para manipular conteudo no DOM (atravez do document), tanto para tags html/class/id
+
 var title = document.querySelector('#title');
 title.textContent = "Aparecida Nutricionista"
