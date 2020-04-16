53ae5b73059b37a73b34068d1b0e1d892dfb145a
marjoritamise <marjori.tamise@gmail.com>
2020-04-16 10:32:16 -0300

    bkup

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@
 // querySelector serve para manipular conteudo no DOM (atravez do document), tanto para tags html/class/id
 // variavel.style.backgroundColor e variavel.style.color => mudando estilo direto pelo JS
 
-// var title = document.querySelector('#title');
-// title.textContent = "Aparecida Nutricionista";
+var title = document.querySelector('#title');
+title.textContent = "Aparecida Nutricionista";
 
 var pacientes = document.querySelectorAll(".paciente");
 
@@ -34,3 +34,36 @@ for (var i = 0; i < pacientes.length; i++) {
         paciente.querySelector(".info-imc").textContent = imc.toFixed(2);
     }
 }
+
+var addPaciente = document.querySelector("#adicionar-paciente")
+
+addPaciente.addEventListener("click", function (event) {
+    event.preventDefault();
+    var form = document.querySelector('#form-adiciona'),
+        nome = form.nome.value,
+        peso = form.peso.value,
+        altura = form.altura.value,
+        gordura = form.gordura.value,
+        tdNome = document.createElement("td"),
+        tdPeso = document.createElement("td"),
+        tdAltura = document.createElement("td"),
+        tdGordura = document.createElement("td"),
+        tdIMC = document.createElement("td"),
+        newTr = document.createElement("tr"),
+        tabela = document.querySelector("#tabela-pacientes");
+
+    tdNome.textContent = nome;
+    tdPeso.textContent = peso;
+    tdAltura.textContent = altura;
+    tdGordura.textContent = gordura;
+
+    newTr.appendChild(tdNome);
+    newTr.appendChild(tdPeso);
+    newTr.appendChild(tdAltura);
+    newTr.appendChild(tdGordura);
+    newTr.appendChild(tdIMC);
+
+    tabela.appendChild(newTr)
+
+
+});
